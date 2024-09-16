ee2260ea1cce9375687c8bd86c1c274f70095229
youssefrramdan <youfahmed353@gmail.com>
2024-09-16 23:52:12 +0300

    problem 7

---
+++ Opposite number.js
@@ -0,0 +1,9 @@
+// ! problem : Very simple, given a number find its opposite (additive inverse).
+
+function opposite(number) {
+  return -number;
+}
+
+console.log(opposite(5));
+console.log(opposite(-3));
+console.log(opposite(0));
